8accc98d7cbdc50ed98a0cfd6602911ab76bc0ce
MOHAMEDMEDOHEMEDA <94225644+MOHAMEDMEDOHEMEDA@users.noreply.github.com>
2026-06-11 00:28:53 +0300

    Upload the app

---
+++ Titanic/TitanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitanicApp.swift
+//  Titanic
+//
+//  Created by Mohamed Magdy on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TitanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
